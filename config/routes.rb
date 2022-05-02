32f8080b515486a4f165717c3638eccbf0cdedc5
takekai822 <kaikisoccer78@gmail.com>
2022-05-02 02:05:07 +0000

    [Finish]follow

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,11 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :books, only: [:index,:show,:edit,:create,:destroy,:update]
-  resources :users, only: [:index,:show,:edit,:update]
+  resources :users, only: [:index,:show,:edit,:update] do
+    resource :relationships, only: [:create, :destroy]
+    get 'followers' => 'relationships#followers', as: 'followers'
+    get 'followings' => 'relationships#followings', as: "followings"
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
 end
