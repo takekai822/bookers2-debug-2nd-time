35974d3d258f0bd5af44bfcd84028ab01534eeda
takekai822 <kaikisoccer78@gmail.com>
2022-05-02 02:21:21 +0000

    [Finish]favorite

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,10 @@ Rails.application.routes.draw do
   get "home/about"=>"homes#about"
   devise_for :users
 
-  resources :books, only: [:index,:show,:edit,:create,:destroy,:update]
+  resources :books, only: [:index,:show,:edit,:create,:destroy,:update] do
+    resource :favorites, only: [:create, :destroy]
+  end
+  
   resources :users, only: [:index,:show,:edit,:update] do
     resource :relationships, only: [:create, :destroy]
     get 'followers' => 'relationships#followers', as: 'followers'
