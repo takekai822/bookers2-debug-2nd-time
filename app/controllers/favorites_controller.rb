08a85827515fbf53719aa4650fd20ce1c33f8691
takekai822 <kaikisoccer78@gmail.com>
2022-05-02 02:29:53 +0000

    [Finish]favorite ajax

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,16 +1,16 @@
 class FavoritesController < ApplicationController
-  
+
   def create
-    book = Book.find(params[:book_id])
-    favorite = current_user.favorites.new(book_id: book.id)
-    favorite.save
-    redirect_to request.referer
+    @book = Book.find(params[:book_id])
+    @favorite = current_user.favorites.new(book_id: @book.id)
+    @favorite.save
+    render 'favorites'
   end
-  
+
   def destroy
-    book = Book.find(params[:book_id])
-    favorite = current_user.favorites.find_by(book_id: book.id).destroy
-    redirect_to request.referer
+    @book = Book.find(params[:book_id])
+    @favorite = current_user.favorites.find_by(book_id: @book.id).destroy
+    render 'favorites'
   end
-  
+
 end
